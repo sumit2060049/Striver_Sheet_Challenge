bce8e889392793b647bd5c805f7786b9f6754ece
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-06 11:44:16 +0530

    Maximum subarray

--- Day1.cpp
+++ Day1.cpp
@@ -94,3 +94,25 @@ public:
         }
     }
 };
+//53. Maximum Subarray
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int n=nums.size();
+    int max=INT_MIN;
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=nums[i];
+        if(max<sum)
+        {
+            max=sum;
+        }
+        if(sum<0)
+            sum=0;
+    }
+    return max;   
+    }
+};
+
