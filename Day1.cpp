1895708314c6232d046417a4ad38999180fd6a4a
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-06 19:12:07 +0530

    sort 0,1,2

--- Day1.cpp
+++ Day1.cpp
@@ -116,3 +116,36 @@ public:
     }
 };
 
+//75. Sort Colors
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int l=0;
+        int mid=0;
+        int h=n-1;
+        while(mid<=h)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[l],nums[mid]);
+                l++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[h]);
+                h--;
+            }
+
+
+        }
+        
+    }
+};
+
