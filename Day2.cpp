626a21d74c953e1c19ab1268698593b4bfc3e3e0
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-08 12:15:33 +0530

    Find the Duplicate Number

--- Day2.cpp
+++ Day2.cpp
@@ -87,3 +87,24 @@ public:
         }
     }
 };
+
+//287. Find the Duplicate Number
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+        
+    }
+};
