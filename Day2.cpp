c6f57a5b202baa04a7916ad766a288f2132a4165
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-08 11:12:00 +0530

    Merge interval

--- Day2.cpp
+++ Day2.cpp
@@ -17,3 +17,27 @@ public:
         
     }
 };
+//56. Merge Intervals
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0)
+            return {};
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> res;
+        int n=intervals.size();
+        for(int i=0;i<n;i++)
+        {
+            if(i==n-1)
+                res.push_back(intervals[i]);
+            else if(intervals[i][1]>=intervals[i+1][0])
+            {
+                intervals[i+1][0]=intervals[i][0];
+                intervals[i+1][1]=max(intervals[i][1],intervals[i+1][1]);
+            }
+            else
+                res.push_back(intervals[i]);
+        }
+        return res;
+    }
+};
