3ae43956f45fc5db3b43927a122072d017e45ea5
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-10 14:26:53 +0530

    Majority element

--- Day3.cpp
+++ Day3.cpp
@@ -55,3 +55,26 @@ public:
         
     }
 };
+
+//169. Majority Element
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+    int count=0,element=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0){
+                    element=nums[i];
+            }
+            if(element==nums[i])
+            {
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return element;
+    }
+};
