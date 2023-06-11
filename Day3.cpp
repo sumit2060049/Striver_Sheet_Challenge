9d113bd4339ae52a5295016dbeb8b6fb26107140
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-11 18:30:30 +0530

    Reverse Pairs

--- Day3.cpp
+++ Day3.cpp
@@ -154,3 +154,68 @@ int uniquePaths(int m, int n) {
         int ans=f(m-1,n-1,dp);
         return ans;
 }
+
+//493. Reverse Pairs
+
+class Solution {
+    void merge(vector<int>&arr,int low ,int mid,int high){	
+	vector<int>temp;
+	int left=low;
+	int right=mid+1;
+
+	while(left<=mid && right<=high){
+		if(arr[left]<=arr[right]){
+			temp.push_back(arr[left]);
+			left++;
+		}
+		else {
+			temp.push_back(arr[right]);
+			right++;
+		}
+	}
+	while(left<=mid){
+		temp.push_back(arr[left]);
+		left++;
+	}
+	while(right<=high){
+		temp.push_back(arr[right]);
+		right++;
+	}
+
+	for(int i=low;i<=high;i++)
+	{
+		arr[i]=temp[i-low];
+	}
+	
+}
+long long countPairs(vector<int>&arr,int low,int mid,int high)
+{	int right=mid+1;
+	int cnt=0;
+	for(int i=low;i<=mid;i++){
+		while(right<=high && arr[i]>2LL*arr[right]){//LL is to typecast.
+			right++;
+		}
+		cnt+=(right-(mid+1));
+	}
+	return cnt;
+}
+long long mergeSort(vector<int>&arr,int low,int high)
+{	long long cnt=0;
+	if(low>=high)
+		return cnt;
+	long long mid=(low+high)/2;
+	cnt+=mergeSort(arr,low,mid);
+	cnt+=mergeSort(arr,mid+1,high);
+	cnt+=countPairs(arr,low,mid,high);
+	merge(arr,low,mid,high);
+	return cnt;
+	
+
+}
+public:
+    int reversePairs(vector<int>& nums) {
+        long long n=nums.size();
+        return (int)mergeSort(nums,0,n-1);
+
+    }
+};
