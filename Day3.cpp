7b0f476e71c7edb66c5a9943743dde2150c89db9
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-06-11 18:29:19 +0530

    unique paths

--- Day3.cpp
+++ Day3.cpp
@@ -130,3 +130,27 @@ public:
 
     }
 };
+
+//Unique Paths
+
+    int f(int i,int j,vector<vector<int>> &dp)
+    {
+        if(i==0 && j==0)
+            return 1;
+        else if(i<0||j<0)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        int x=f(i-1,j,dp);
+        int y=f(i,j-1,dp);
+
+        return dp[i][j]=x+y;
+    }
+
+int uniquePaths(int m, int n) {
+
+	vector<vector<int>>dp(m,vector<int>(n,-1));
+        int ans=f(m-1,n-1,dp);
+        return ans;
+}
